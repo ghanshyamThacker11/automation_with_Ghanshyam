e98b266d5c744e8c5b03d4292d188b875a7f86d3
ghanshyamThacker11 <ghanshyam.thacker@simformsolutions.com>
2026-06-03 13:50:23 +0530

    Increase action timeout to 12000ms and remove test.only from window.spec.js

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    actionTimeout: 9000
+    actionTimeout: 12000,
   },
 
   /* Configure projects for major browsers */
