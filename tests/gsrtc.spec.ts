e9e7ada587bd4a2d92d783ecb60f2e972e5342f7
ghanshyamThacker11 <ghanshyam.thacker@simformsolutions.com>
2026-06-18 17:39:11 +0530

    Added Real world test suite (GSRTC)

---
+++ tests/gsrtc.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('Date Picker', async ({ page }) => {
+    await page.goto('https://gsrtc.in/site/');
+    await page.getByRole('button', { name: 'Close' }).click();
+    await page.getByRole('textbox', { name: 'Select Date' }).click();
+
+    let text = (await page.locator(".ui-datepicker-title").textContent())?.trim();
+    let month = await page.locator('.ui-datepicker-month').textContent();
+    let year = await page.locator('.ui-datepicker-year').textContent();
+    let date = (month?.trim() ?? '') + (year?.trim() ?? '');
+    while (date !== "July2026") {
+        await page.locator('a[title="Next"]').click();
+
+        month = await page.locator('.ui-datepicker-month').textContent();
+        year = await page.locator('.ui-datepicker-year').textContent();
+
+        date = (month?.trim() ?? '') + (year?.trim() ?? '');
+    }
+    await page.locator("#ui-datepicker-div").getByRole('link', { name: '1', exact: true }).click();
+});
